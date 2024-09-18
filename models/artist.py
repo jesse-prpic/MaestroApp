efd89dfb4786b6405bc0ed13db0e2c37f3e48dee
jesse-prpic <13812@coderacademy.edu.au>
2024-09-18 23:54:09 +1000

    created the schema, relationships and classes for model Artis

--- models/artist.py
+++ models/artist.py
@@ -1,3 +1,16 @@
 from init import db, ma
 from marshmallow import fields
 
+class Artist(db.Model):
+    __tablename__ = "artists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    songs = db.relationship('Song', back_populates='artist')
+
+class ArtistSchema(ma.Schema):
+    songs = fields.List(fields.Nested('SongSchema', only=["id", "title"]))
+
+class Meta:
+    fields = ("id", "name", "songs")
+    ordered = True
